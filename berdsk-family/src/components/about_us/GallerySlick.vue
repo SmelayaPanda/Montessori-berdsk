51164721d2e190d47521765b156c87fcba53ff4d
panda <SmelayaPanda@mail.ru>
2018-01-24 22:02:18 +0700

    update images + card border

--- berdsk-family/src/components/about_us/GallerySlick.vue
+++ berdsk-family/src/components/about_us/GallerySlick.vue
@@ -36,7 +36,7 @@
         </slide>
         <slide :index="5">
           <span class="title mb-3">Разновозрастные группа</span>
-          <img src="@/assets/img/about_us_main/razniiVozrast.jpg"/>
+          <img src="@/assets/img/about_us_main/RazniiVozrast.jpeg"/>
         </slide>
       </carousel-3d>
     </div>
