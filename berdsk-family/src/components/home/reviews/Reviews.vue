510c69224d818cd8f1b850dcdcebb51b9949e37a
panda <SmelayaPanda@mail.ru>
2018-02-05 22:53:29 +0700

    reviews

--- berdsk-family/src/components/home/reviews/Reviews.vue
+++ berdsk-family/src/components/home/reviews/Reviews.vue
@@ -1,37 +1,61 @@
 <template>
-  <v-container class="review_container">
-    <h2 class="review_header secondary--text text-xs-center">Что говорят о нас</h2>
-    <h4 class="review_header primary--text text-xs-center">Отзывы клиентов</h4>
-    <div class="review_text_container">
-      <p class="review_text secondary--text">
-        {{ selectedReview }}
-      </p>
-    </div>
+  <div>
 
-    <div class="review_icons_container">
+    <img src="../../../assets/img/about_us_home/about_say.png" height="155" width="461" class="about_say_title"/>
+
+    <v-container class="review_container">
+
+      <v-layout class="review_text_container">
+        <v-flex xs3>
+          <img src="@/assets/img/about_us_home/back.png"
+               height="138" width="71"
+               class="back_btn"
+          />
+        </v-flex>
+
+
+        <v-flex xs6>
+          <p class="quotes back_quotes">“</p>
+          <div class="text_block">
+            <p class="review_text secondary--text">{{ selectedReview }}</p>
+          </div>
+          <p class="quotes forward_quotes">”</p>
+        </v-flex>
+
+        <v-flex xs3>
+          <img src="@/assets/img/about_us_home/forward.png"
+               height="127" width="57"
+               class="forward_btn"
+          />
+        </v-flex>
+      </v-layout>
+
+
       <v-layout row wrap>
-        <v-flex xs4>
+        <v-flex xs4 style="margin-left: 60px"></v-flex>
+        <v-flex xs1>
           <img ref="rw0"
                class="review_icon rw0"
                @click="changeReview(0)"
                src="@/assets/img/about_us_main/svo.jpg"/>
         </v-flex>
-        <v-flex xs4>
+        <v-flex xs1>
           <img ref="rw1"
                class="review_icon rw1"
                @click="changeReview(1)"
                src="@/assets/img/about_us_main/VpitivUm.jpg"/>
         </v-flex>
-        <v-flex xs4>
+        <v-flex xs1>
           <img ref="rw2"
                class="review_icon rw2"
                @click="changeReview(2)"
                src="@/assets/img/about_us_main/UvazhenieLichnosti.jpg"/>
         </v-flex>
+        <v-flex xs4></v-flex>
       </v-layout>
-    </div>
+    </v-container>
 
-  </v-container>
+  </div>
 </template>
 
 <script>
@@ -57,7 +81,7 @@
           this.$refs['rw' + i].style.transform = 'scale(1)'
         }
         this.$refs['rw' + ord].style.opacity = 1
-        this.$refs['rw' + ord].style.transform = 'scale(1.2)'
+        this.$refs['rw' + ord].style.transform = 'scale(1.25)'
       }
     }
   }
@@ -74,9 +98,8 @@
   }
 
   .review_container {
-    width: 350px;
     height: 500px;
-    padding-top: 60px;
+    padding-top: 10px;
   }
 
   .review_icon {
@@ -85,31 +108,72 @@
     border: 2px solid #bced96;
     border-radius: 100px;
     transition: all 0.5s;
-
   }
 
   .review_icon:hover {
     cursor: pointer;
   }
 
-  .review_header {
-    font-weight: bolder;
-  }
-
-  .review_icons_container {
-    width: 280px;
-    margin-left: 15px;
-  }
-
   .review_text {
+    text-align: center;
     font-weight: normal;
     height: 130px;
     margin-top: 10px;
   }
 
   .review_text_container {
-    width: 500px;
-    margin-left: -100px;
     text-align: center;
+  }
+
+  .about_say_title {
+    transform: scale(0.8);
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+  }
+
+  .back_btn {
+    opacity: 0.8;
+    transition: all 0.5s;
+  }
+
+  .forward_btn {
+    opacity: 0.8;
+    transition: all 0.5s;
+  }
+
+  .back_btn:hover {
+    cursor: pointer;
+    opacity: 1;
+  }
+
+  .forward_btn:hover {
+    cursor: pointer;
+    opacity: 1;
+  }
+
+  .quotes {
+    font-size: 72px;
+    font-weight: bold;
+    color: rgb(188, 237, 150);
+    line-height: 0.333;
+    text-align: center;
+    z-index: 329;
+  }
+
+  .back_quotes {
+    text-align: left;
+    margin: 0;
+  }
+
+  .forward_quotes {
+    text-align: right;
+    margin: 0;
+  }
+
+  .text_block {
+    display: flex !important;
+    justify-content: center !important; /* align horizontal */
+    align-items: center !important;
   }
 </style>
